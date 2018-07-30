41fcda5d2661c85f8ad60809054069dd9ba361d8
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-30 15:02:51 +0800

    finish checkFourthSpeedRecordWasSetbyAnAstonMartin

--- src/test/java/exercises/RestAssuredExercises5Test.java
+++ src/test/java/exercises/RestAssuredExercises5Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class RestAssuredExercises5Test {
@@ -53,7 +54,8 @@ public class RestAssuredExercises5Test {
 		given().
 			spec(requestSpec).
 		when().
-		then();
+				get("/xml/speedrecords").
+		then().body("speedRecords.car[3].@make",equalTo("Aston Martin"));
 	}
 	
 	/*******************************************************
